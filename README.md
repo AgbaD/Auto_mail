edd0bbfec5947ed764307696c40b25822c1db501
Damilare <blankgodd33@gmail.com>
2020-09-08 17:14:03 +0100

    Done. I haven't tested yet

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Auto_mail
 Send personalised emails to multiple addresses at once 
+
+## Usage
+- Provide a csv file the consists of names and emails of recepients
+  e.g name,email
+      john,john@example.com
+      mike,mike@try.com
+- Provide a text file that contains the email. The personalized content, eg name,
+the place holder "{name}" should be used.
+- (Optional) Provide a html file. This helps for adding fancy. The personalized content, eg name,
+the place holder "{name}" should be used.
+- All files provided ,ust be in the same directory with the script
+- Run the script and input your(the sender) email and then your password
+- Input the email subject
+- Then watch the script run
+
+### Enjoy :smile:
---
+++ app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+# Author:   @BlankGodd_
+
+import csv
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from getpass import getpass
+
+port = 465
+smtp_server = "smtp.gmail.com"
+sender_email = ""
+
+def send_bulk_mail():
+    print("Input password")
+    password = getpass()
+
+    subject = input('Enter email subject: ')
+
+    # read text file
+    with open("email.txt") as e_txt:
+        text = e_txt.read()
+
+    try:
+        # read html file
+        with open("email.html") as e_html:
+            html = e_html.read()
+    except:
+        pass
+
+    # open subscribers file
+    with open("subscribers.csv") as file:
+        item = csv.reader(file)
+        next(item)  # Skip header row
+        for name, receiver_email in item:
+
+            message = MIMEMultipart("alternative")
+            message["Subject"] = subject
+            message["From"] = sender_email
+            message["To"] = receiver_email
+
+            this_text = text.format('name')
+            try:
+                this_html = html.format('name')
+            except:
+                pass
+
+            part1 = MIMEText(this_text, "plain")
+            try:
+                part2 = MIMEText(this_html, "html")
+            except:
+                pass
+
+            message.attach(part1)
+            try:
+                message.attach(part2)
+            except:
+                pass
+
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                server.login(sender_email, password)
+                server.sendmail(
+                    sender_email, receiver_email, message.as_string()
+                )
+
+            print(f"Sending email to {name}")
+
+
+if __name__ == "__main__":
+    send_bulk_mail()
